7de88c9137ca11792b1d6eed602ed64251f8e68e
Yamkelani Velaphi <106350341+YamVel22@users.noreply.github.com>
2024-03-01 10:35:52 +0200

    update

---
+++ function.js
@@ -0,0 +1,21 @@
+//create a book constructor function
+function book(title, author, year) {
+  this.title = title;
+  this.author;
+  this.year;
+}
+
+//create a author constructor function
+function author(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.book = [];
+}
+
+//adding a method to author prototype
+Author.prototype.addBook = function (book) {
+    this.book.push(book);
+}
+
+
+
